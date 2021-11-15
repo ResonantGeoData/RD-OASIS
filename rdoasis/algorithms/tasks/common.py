1f22f274f49d9544e8502e1300e36ace74f56687
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-11-15 18:14:52 -0500

    Add docker image file loading

--- rdoasis/algorithms/tasks/common.py
+++ rdoasis/algorithms/tasks/common.py
@@ -7,6 +7,7 @@ from typing import List, Set
 from billiard.einfo import ExceptionInfo
 import celery
 from django.core.files.uploadedfile import SimpleUploadedFile
+import docker
 from rgd.models.common import ChecksumFile
 
 from rdoasis.algorithms.models import Algorithm, AlgorithmTask, Dataset
@@ -45,6 +46,31 @@ class ManagedTask(celery.Task):
         for checksum_file in files:
             self.input_dataset_paths.append(checksum_file.download_to_local_path(self.input_dir))
 
+    def _maybe_download_docker_image_file(self):
+        """Download the uploaded docker image file, if it exists."""
+        # Default to None, in case it's not used
+        self.docker_image_file_path = None
+        self.docker_image_file_id = None
+        if self.algorithm.docker_image.image_file is None:
+            return
+
+        docker_image_file: ChecksumFile = self.algorithm.docker_image.image_file
+
+        # Namespace
+        docker_dir = self.root_dir / 'docker'
+        docker_dir.mkdir()
+
+        # Set path of downloaded docker image file
+        self.docker_image_file_path = docker_image_file.download_to_local_path(docker_dir)
+
+        # Load image
+        client = docker.from_env()
+        with open(self.docker_image_file_path, 'rb') as f:
+            result = client.images.load(f.read())
+
+        # Set ID of loaded docker image
+        self.docker_image_file_id = result[0].id
+
     def _create_directories(self):
         # Create root dir
         self.root_dir = Path(tempfile.mkdtemp())
@@ -73,6 +99,9 @@ class ManagedTask(celery.Task):
 
         # Download input
         self._download_input_dataset()
+
+        # Download uploaded docker image if necessary
+        self._maybe_download_docker_image_file()
 
     def _cleanup(self):
         """Perform any necessary cleanup."""
--- rdoasis/algorithms/tests/test_tasks_docker.py
+++ rdoasis/algorithms/tests/test_tasks_docker.py
@@ -1,9 +1,44 @@
+import tempfile
 from typing import List
 
+from django.core.files.uploadedfile import SimpleUploadedFile
+import docker
 import pytest
 from rgd.models.common import ChecksumFile
 
-from rdoasis.algorithms.models import Algorithm, AlgorithmTask
+from rdoasis.algorithms.models import Algorithm, AlgorithmTask, DockerImage
+
+# First command fails immediately, second command runs
+# successfully but outputs a nonzero exit status
+FAILING_COMMANDS = ['notacommand', 'exit 1']
+ECHO_AND_WRITE_CMD = '/bin/sh -c "echo DATA > output/hello.txt && echo HI"'
+
+
+@pytest.fixture
+def saved_docker_image() -> str:
+    client = docker.from_env()
+    image = client.images.pull('alpine')
+    with tempfile.NamedTemporaryFile(suffix='.tar') as f:
+        # Save image to file
+        for chunk in image.save(named=True):
+            f.write(chunk)
+
+        f.seek(0)
+        yield f.name
+
+
+@pytest.fixture
+def docker_image_from_file(
+    saved_docker_image, docker_image_factory, checksum_file_factory
+) -> DockerImage:
+    with open(saved_docker_image, 'rb') as f:
+        file = SimpleUploadedFile(name=saved_docker_image, content=f.read())
+
+    return docker_image_factory(
+        name='test',
+        image_id=None,
+        image_file=checksum_file_factory(name='alpine_image', file=file),
+    )
 
 
 @pytest.mark.docker
@@ -11,7 +46,7 @@ from rdoasis.algorithms.models import Algorithm, AlgorithmTask
 def test_successful_task(algorithm_factory, docker_image_factory, dataset_factory):
     alg: Algorithm = algorithm_factory(
         name='test',
-        command='/bin/sh -c "echo DATA > output/hello.txt && echo HI"',
+        command=ECHO_AND_WRITE_CMD,
         docker_image=docker_image_factory(name='test', image_id='alpine'),
     )
 
@@ -33,11 +68,57 @@ def test_successful_task(algorithm_factory, docker_image_factory, dataset_factor
 
 @pytest.mark.docker
 @pytest.mark.django_db
-def test_failed_task(algorithm_factory, docker_image_factory, dataset_factory):
+def test_successful_task_image_file(algorithm_factory, docker_image_from_file, dataset_factory):
+    alg: Algorithm = algorithm_factory(
+        name='test', command=ECHO_AND_WRITE_CMD, docker_image=docker_image_from_file
+    )
+
+    # Run task
+    task: AlgorithmTask = alg.run(dataset_id=dataset_factory().id)
+    task.refresh_from_db()
+
+    # Make assertions
+    assert task.status == AlgorithmTask.Status.SUCCEEDED
+    assert task.output_log == 'HI\n'
+
+    files: List[ChecksumFile] = list(task.output_dataset.files.all())
+    assert len(files) == 1
+    assert files[0].name == 'hello.txt'
+
+    file = files[0]
+    assert file.file.read() == b'DATA\n'
+
+
+@pytest.mark.docker
+@pytest.mark.django_db
+@pytest.mark.parametrize('command', FAILING_COMMANDS)
+def test_failed_task(algorithm_factory, docker_image_factory, dataset_factory, command):
     alg: Algorithm = algorithm_factory(
         name='test',
-        command='notacommand',
+        command=command,
         docker_image=docker_image_factory(name='test', image_id='alpine'),
+    )
+
+    # Run task
+    task: AlgorithmTask = alg.run(dataset_id=dataset_factory().id)
+    task.refresh_from_db()
+
+    # Make assertions
+    assert task.status == AlgorithmTask.Status.FAILED
+    assert task.output_dataset is None or not task.output_dataset.files.count()
+
+    # Assert output log isn't empty
+    assert task.output_log
+
+
+@pytest.mark.docker
+@pytest.mark.django_db
+@pytest.mark.parametrize('command', FAILING_COMMANDS)
+def test_failed_task_image_file(
+    algorithm_factory, docker_image_from_file, dataset_factory, command
+):
+    alg: Algorithm = algorithm_factory(
+        name='test', command=command, docker_image=docker_image_from_file
     )
 
     # Run task
