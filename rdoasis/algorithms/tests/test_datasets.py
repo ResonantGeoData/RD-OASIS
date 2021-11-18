ca7cab5d84ac05d4b9cbdecea936873ffce45e38
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-11-18 16:40:58 -0500

    Fix failing test

--- rdoasis/algorithms/tests/test_datasets.py
+++ rdoasis/algorithms/tests/test_datasets.py
@@ -22,20 +22,19 @@ def test_rest_dataset_update_files(dataset, checksum_file_factory, authenticated
     assert not set(files_to_remove) & set(r.json()['files'])
 
 
-# FIXME: This test fails and I have no idea why
-# @pytest.mark.django_db
-# def test_rest_dataset_update_files_invalid(dataset, authenticated_api_client):
-#     # Try to insert file that doesn't exist
-#     invalid_file_id = -1
-#     r = authenticated_api_client.put(
-#         f'/api/datasets/{dataset.id}/files/',
-#         {
-#             'insert': [invalid_file_id],
-#             'delete': [],
-#         },
-#         format='json',
-#     )
+@pytest.mark.django_db(transaction=True)
+def test_rest_dataset_update_files_invalid(dataset, authenticated_api_client):
+    """Try to insert file that doesn't exist."""
+    invalid_file_id = -1
+    r = authenticated_api_client.put(
+        f'/api/datasets/{dataset.id}/files/',
+        {
+            'insert': [invalid_file_id],
+            'delete': [],
+        },
+        format='json',
+    )
 
-#     assert r.status_code == 400
-#     assert type(r.json()['insert']) == 'str'
-#     assert invalid_file_id not in [f.id for f in dataset.files.all()]
+    assert r.status_code == 400
+    assert type(r.json()['insert']) == str
+    assert invalid_file_id not in [f.id for f in dataset.files.all()]
