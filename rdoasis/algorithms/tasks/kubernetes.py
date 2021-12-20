af75be754f933e55f2eaa61a374dce8f120657af
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-12-20 16:41:27 -0500

    Use init containers instead of lifecycle hook

--- rdoasis/algorithms/tasks/kubernetes.py
+++ rdoasis/algorithms/tasks/kubernetes.py
@@ -24,98 +24,109 @@ class ManagedK8sTask(celery.Task):
         # Set algorithm
         self.algorithm: Algorithm = self.algorithm_task.algorithm
 
+        # Define job vars
+        self.temp_path = Path(tempfile.mkdtemp())
+        self.job_name = f'algorithm-{self.algorithm.pk}-task-{self.algorithm_task.pk}'
+        self.image_id = self.algorithm_task.algorithm.docker_image.image_id
+        self.main_container_name = re.sub(r'[^a-zA-Z\d-]', '-', self.image_id.lower())
+
+    def _sidecar_container_env_vars(self):
+        from kubernetes import client, config
+
+        # Load config from outside of k8s cluster
+        config.load_kube_config()
+
+        return [
+            client.V1EnvVar(
+                name='DJANGO_DATABASE_URL',
+                value='postgres://postgres:postgres@host.minikube.internal:5432/django',
+            ),
+            client.V1EnvVar(
+                name='DJANGO_CELERY_BROKER_URL',
+                value='amqp://host.minikube.internal:5672/',
+            ),
+            client.V1EnvVar(
+                name='DJANGO_MINIO_STORAGE_ENDPOINT',
+                value='host.minikube.internal:9000',
+            ),
+            #
+            # Remain the same
+            client.V1EnvVar(
+                name='DJANGO_CONFIGURATION',
+                value=os.environ['DJANGO_CONFIGURATION'],
+            ),
+            client.V1EnvVar(
+                name='DJANGO_MINIO_STORAGE_ACCESS_KEY',
+                value=os.environ['DJANGO_MINIO_STORAGE_ACCESS_KEY'],
+            ),
+            client.V1EnvVar(
+                name='DJANGO_MINIO_STORAGE_SECRET_KEY',
+                value=os.environ['DJANGO_MINIO_STORAGE_SECRET_KEY'],
+            ),
+            client.V1EnvVar(
+                name='DJANGO_STORAGE_BUCKET_NAME',
+                value=os.environ['DJANGO_STORAGE_BUCKET_NAME'],
+            ),
+            #
+            # Extra envs
+            client.V1EnvVar(name='JOB_NAME', value=self.job_name),
+            client.V1EnvVar(name='CONTAINER_NAME', value=self.main_container_name),
+            client.V1EnvVar(name='TASK_ID', value=str(self.algorithm_task.pk)),
+            client.V1EnvVar(name='TEMP_DIR', value=str(self.temp_path)),
+        ]
+
     def _construct_job(self):
         from kubernetes import client, config
 
         # Load config from outside of k8s cluster
         config.load_kube_config()
 
-        # Define volume
-        volume = client.V1Volume(
-            name='task-data',
-            empty_dir=client.V1EmptyDirVolumeSource(),
-        )
+        # Define volume mount
+        volume_mount = client.V1VolumeMount(name='task-data', mount_path=str(self.temp_path))
 
-        job_name = f'algorithm-{self.algorithm.pk}-task-{self.algorithm_task.pk}'
-        temp_path = Path(tempfile.mkdtemp())
-
-        # Define container with volume mount
-        image_id = self.algorithm_task.algorithm.docker_image.image_id
-        args = shlex.split(self.algorithm_task.algorithm.command)
-        main_container_name = re.sub(r'[^a-zA-Z\d-]', '-', image_id.lower())
+        # Define main container
         main_container = client.V1Container(
-            name=main_container_name,
-            image=image_id,
-            args=args,
-            volume_mounts=[client.V1VolumeMount(name='task-data', mount_path=str(temp_path))],
-            working_dir=str(temp_path),
+            name=self.main_container_name,
+            image=self.image_id,
+            args=shlex.split(self.algorithm_task.algorithm.command),
+            volume_mounts=[volume_mount],
+            working_dir=str(self.temp_path),
             # resources=client.V1ResourceRequirements(limits={'nvidia.com/gpu': 1}),
         )
 
-        monitor_container = client.V1Container(
-            name=f'{main_container_name}--monitor',
+        # Container that will initialize the pod emptyDir with data
+        init_container = client.V1Container(
+            name=f'{self.main_container_name}--init',
             image='oasis-sidecar',
-            volume_mounts=[client.V1VolumeMount(name='task-data', mount_path=str(temp_path))],
+            volume_mounts=[volume_mount],
             image_pull_policy='Never',
-            lifecycle=client.V1Lifecycle(
-                post_start=client.V1Handler(
-                    _exec=client.V1ExecAction(
-                        command=['/opt/django-project/manage.py', 'setup_container_k8s']
-                    ),
-                ),
-            ),
-            env=[
-                # Modified django envs
-                # TODO: Evaluate these for use in AWS cluster
-                client.V1EnvVar(
-                    name='DJANGO_DATABASE_URL',
-                    value='postgres://postgres:postgres@host.minikube.internal:5432/django',
-                ),
-                client.V1EnvVar(
-                    name='DJANGO_CELERY_BROKER_URL',
-                    value='amqp://host.minikube.internal:5672/',
-                ),
-                client.V1EnvVar(
-                    name='DJANGO_MINIO_STORAGE_ENDPOINT',
-                    value='host.minikube.internal:9000',
-                ),
-                #
-                # Remain the same
-                client.V1EnvVar(
-                    name='DJANGO_CONFIGURATION',
-                    value=os.environ['DJANGO_CONFIGURATION'],
-                ),
-                client.V1EnvVar(
-                    name='DJANGO_MINIO_STORAGE_ACCESS_KEY',
-                    value=os.environ['DJANGO_MINIO_STORAGE_ACCESS_KEY'],
-                ),
-                client.V1EnvVar(
-                    name='DJANGO_MINIO_STORAGE_SECRET_KEY',
-                    value=os.environ['DJANGO_MINIO_STORAGE_SECRET_KEY'],
-                ),
-                client.V1EnvVar(
-                    name='DJANGO_STORAGE_BUCKET_NAME',
-                    value=os.environ['DJANGO_STORAGE_BUCKET_NAME'],
-                ),
-                #
-                # Extra envs
-                client.V1EnvVar(name='JOB_NAME', value=job_name),
-                client.V1EnvVar(name='CONTAINER_NAME', value=main_container_name),
-                client.V1EnvVar(name='TASK_ID', value=str(self.algorithm_task.pk)),
-                client.V1EnvVar(name='TEMP_DIR', value=str(temp_path)),
-            ],
-            # resources=client.V1ResourceRequirements(limits={'nvidia.com/gpu': 1}),
+            command=['/opt/django-project/manage.py'],
+            args=['setup_container_k8s'],
+            env=self._sidecar_container_env_vars(),
+        )
+
+        # Container that will monitor the main container and upload the resulting logs & data
+        monitor_container = client.V1Container(
+            name=f'{self.main_container_name}--monitor',
+            image='oasis-sidecar',
+            volume_mounts=[volume_mount],
+            image_pull_policy='Never',
+            env=self._sidecar_container_env_vars(),
         )
 
         # Define job template
         template = client.V1PodTemplateSpec(
-            metadata=client.V1ObjectMeta(name=job_name),
+            metadata=client.V1ObjectMeta(name=self.job_name),
             spec=client.V1PodSpec(
                 restart_policy="Never",
-                # Monitor container must be defined first, so the post_start lifecycle hook
-                # delays the main container from starting
+                init_containers=[init_container],
                 containers=[monitor_container, main_container],
-                volumes=[volume],
+                volumes=[
+                    client.V1Volume(
+                        name='task-data',
+                        empty_dir=client.V1EmptyDirVolumeSource(),
+                    ),
+                ],
                 service_account_name='job-robot',
             ),
         )
@@ -124,7 +135,7 @@ class ManagedK8sTask(celery.Task):
         return client.V1Job(
             api_version="batch/v1",
             kind="Job",
-            metadata=client.V1ObjectMeta(name=job_name),
+            metadata=client.V1ObjectMeta(name=self.job_name),
             spec=client.V1JobSpec(
                 template=template,
                 backoff_limit=0,
