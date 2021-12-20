e2d98853268df19472dcac6a9848a84fd552e611
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-12-20 17:27:38 -0500

    Allow running tasks with kubernetes (#26)

---
+++ rdoasis/algorithms/management/commands/monitor_container_k8s.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+
+from rdoasis.algorithms.management.utils.k8s import KubernetesContainerMonitor
+
+
+class Command(BaseCommand):
+    help = 'Monitor a K8s container and return files, status, etc.'
+
+    def handle(self, *args, **options):
+        # TODO: use post_start lifecycle hook to build local image if necessary
+        KubernetesContainerMonitor.from_env().monitor_container()
