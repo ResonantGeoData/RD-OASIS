d0d87837d09f2ea61ceed760c83848135dc8fdf9
Mike VanDenburgh <37340715+mvandenburgh@users.noreply.github.com>
2022-01-07 16:18:33 -0500

    Add `tree` endpoint to Dataset viewset (#33)
    Co-authored-by: Jacob Nesbitt <jjnesbitt2@gmail.com>

--- rdoasis/algorithms/tests/test_datasets.py
+++ rdoasis/algorithms/tests/test_datasets.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 import pytest
+from rgd.models import ChecksumFile
 
 
 @pytest.mark.django_db(transaction=True)
@@ -38,3 +41,74 @@ def test_rest_dataset_update_files_invalid(dataset, authenticated_api_client):
     assert r.status_code == 400
     assert type(r.json()['insert']) == str
     assert invalid_file_id not in [f.id for f in dataset.files.all()]
+
+
+@pytest.mark.django_db(transaction=True)
+def test_rest_dataset_tree(dataset, checksum_file_factory, admin_api_client):
+    """Test that the tree list endpoint functions as expected."""
+    # Top level
+    file1: ChecksumFile = checksum_file_factory(name='h.txt')
+
+    # a/ directory
+    file2: ChecksumFile = checksum_file_factory(name='a/f.txt')
+    file3: ChecksumFile = checksum_file_factory(name='a/g.txt')
+
+    # a/b/ directory
+    file4: ChecksumFile = checksum_file_factory(name='a/b/d.txt')
+    file5: ChecksumFile = checksum_file_factory(name='a/b/e.txt')
+    file6: ChecksumFile = checksum_file_factory(name='a/b/c.jpg')
+
+    dataset.files.set([file1, file2, file3, file4, file5, file6])
+
+    # Testing folders
+    folder_b = [file4, file5, file6]
+    folder_a = [file2, file3] + folder_b
+
+    known_size_a = sum([f.size for f in folder_a if f.size is not None])
+    a_created = min([file.created for file in folder_a]).isoformat().replace('+00:00', 'Z')
+    a_modified = max([file.modified for file in folder_a]).isoformat().replace('+00:00', 'Z')
+
+    known_size_b = sum([f.size for f in folder_b if f.size is not None])
+    b_created = min([file.created for file in folder_b]).isoformat().replace('+00:00', 'Z')
+    b_modified = max([file.modified for file in folder_b]).isoformat().replace('+00:00', 'Z')
+
+    # Check top level response
+    r = admin_api_client.get(f'/api/datasets/{dataset.id}/tree/')
+    assert r.json()['files'][file1.name]['id'] == file1.id
+    assert r.json()['folders'] == {
+        'a': {
+            'known_size': known_size_a,
+            'num_files': len(folder_a),
+            'num_url_files': 0,
+            'created': a_created,
+            'modified': a_modified,
+        }
+    }
+
+    # Search folder 'a'
+    r = admin_api_client.get(f'/api/datasets/{dataset.id}/tree/', {'path_prefix': 'a'})
+
+    # Files
+    files = r.json()['files']
+    assert files[Path(file2.name).name]['id'] == file2.id
+    assert files[Path(file3.name).name]['id'] == file3.id
+
+    # Folders
+    assert r.json()['folders'] == {
+        'b': {
+            'known_size': known_size_b,
+            'num_files': len(folder_b),
+            'num_url_files': 0,
+            'created': b_created,
+            'modified': b_modified,
+        }
+    }
+
+    # Search folder 'a/b'
+    r = admin_api_client.get(f'/api/datasets/{dataset.id}/tree/', {'path_prefix': 'a/b'})
+
+    # Files
+    files = r.json()['files']
+    assert files[Path(file4.name).name]['id'] == file4.id
+    assert files[Path(file5.name).name]['id'] == file5.id
+    assert files[Path(file6.name).name]['id'] == file6.id
